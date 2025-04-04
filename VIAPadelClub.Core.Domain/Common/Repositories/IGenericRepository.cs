90611ce560a272730f3e8c62c3d8aa3cfe34a575
rajipkanxo01 <pdlrajip12@gmail.com>
2025-04-04 17:11:11 +0200

    Add generic repository base and interface for aggregate roots

---
+++ VIAPadelClub.Core.Domain/Common/Repositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using VIAPadelClub.Core.Domain.Common.BaseClasses;
+using VIAPadelClub.Core.Tools.OperationResult;
+
+namespace VIAPadelClub.Core.Domain.Common.Repositories;
+
+public interface IGenericRepository<TAggr, in TId> where TAggr: AggregateRoot 
+{
+    Task<Result<TAggr>> GetAsync(TId id);
+    Task<Result> RemoveAsync(TId id);
+    Task<Result> AddAsync(TAggr aggregate);
+}
